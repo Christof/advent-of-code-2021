0fd3f6f05218dbd07cd41d97b33e152f6b1bdece
christof <christof.sirk@gmail.com>
2021-12-11 21:54:03 +0100

    Add solution of first part of day2 in f#

---
+++ fs/day2.fsx
@@ -0,0 +1,27 @@
+let exampleInput =
+    "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+
+let (|Command|_|) (p: string) (s: string) =
+    if s.StartsWith(p) then
+        Some(s.Substring(p.Length) |> int)
+    else
+        None
+
+let reducer ((x, y): int * int) (line: string) =
+    match line with
+    | Command "forward" amount -> (x + amount, y)
+    | Command "up" amount -> (x, y - amount)
+    | Command "down" amount -> (x, y + amount)
+    | _ -> (x, y)
+
+let (x, y) =
+    exampleInput.Split '\n'
+    |> Array.toList
+    |> List.fold reducer (0, 0)
+
+printf "result is %d" (x * y)
