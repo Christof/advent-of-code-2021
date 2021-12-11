941c52d6dea314501a3b7b6e8ae9cc937229c043
christof <christof.sirk@gmail.com>
2021-12-11 22:14:14 +0100

    Use whole input to get result for day2

--- fs/day2.fsx
+++ fs/day2.fsx
@@ -6,6 +6,10 @@ up 3
 down 8
 forward 2"
 
+let lines = exampleInput.Split '\n'
+
+let input = System.IO.File.ReadLines("fs/day2.txt")
+
 let (|Command|_|) (p: string) (s: string) =
     if s.StartsWith(p) then
         Some(s.Substring(p.Length) |> int)
@@ -19,9 +23,7 @@ let reducer ((x, y): int * int) (line: string) =
     | Command "down" amount -> (x, y + amount)
     | _ -> (x, y)
 
-let (x, y) =
-    exampleInput.Split '\n'
-    |> Array.toList
-    |> List.fold reducer (0, 0)
+let solve (input: seq<string>) =
+    let (x, y) = input |> Seq.fold reducer (0, 0)
 
-printf "result is %d" (x * y)
+    printf "result is %d" (x * y)
