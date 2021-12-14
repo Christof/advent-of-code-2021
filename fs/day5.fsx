936a5f9d55375c44123fec491a482eb0cdbbf238
christof <christof.sirk@gmail.com>
2021-12-14 20:44:58 +0100

    Add solution for part 2

--- fs/day5.fsx
+++ fs/day5.fsx
@@ -94,3 +94,60 @@ let solve (input: seq<string>) =
 
 solve lines
 solve input
+
+let drawLineWithDiag (map: int [,]) (line: Line) =
+    printf "drawLine %A \n" line
+    let ((x1, y1), (x2, y2)) = line
+
+    if (x1 = x2) then
+        getPointSeq y1 y2
+        |> Seq.iter (fun y -> Array2D.set map y x1 (map.[y, x1] + 1))
+
+        map
+    else if (y1 = y2) then
+        getPointSeq x1 x2
+        |> Seq.iter (fun x -> Array2D.set map y1 x (map.[y1, x] + 1))
+
+        map
+    else
+        let diagSeq =
+            Seq.zip (getPointSeq y1 y2) (getPointSeq x1 x2)
+
+        printf "diag Seq %A" diagSeq
+
+        diagSeq
+        |> Seq.iter (fun (y, x) -> Array2D.set map y x (map.[y, x] + 1))
+
+        map
+
+
+
+let solve2 (input: seq<string>) =
+    let lines = parseLines input
+
+    let allPoints =
+        Seq.append (Seq.map fst lines) (Seq.map snd lines)
+
+    let maxX = allPoints |> Seq.map fst |> Seq.max
+    let maxY = allPoints |> Seq.map snd |> Seq.max
+    printf "maxX %d maxY %d\n" maxX maxY
+
+    let map =
+        Array2D.zeroCreate<int> (maxY + 1) (maxX + 1)
+
+    let finalMap =
+        lines
+        |> Seq.fold (fun diagram line -> drawLineWithDiag diagram line) map
+
+    printf "finalMap\n%A\n" finalMap
+
+    let overlapPointsCount =
+        flat2Darray finalMap
+        |> Seq.where (fun cell -> cell >= 2)
+        |> Seq.length
+
+    printf "points %d\n" overlapPointsCount
+    overlapPointsCount
+
+solve2 lines
+solve2 input
