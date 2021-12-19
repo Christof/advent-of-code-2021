c6f927dcb20c03c17ed8e8495fca9fb55394a692
christof <christof.sirk@gmail.com>
2021-12-19 12:11:24 +0100

    Too slow solution for part 2

--- fs/day9.fsx
+++ fs/day9.fsx
@@ -54,10 +54,82 @@ let solve (input: array<string>) =
                 0)
 
     let sumOfRiskLevels = riskLevels |> flat2Darray |> Seq.sum
-    printf "Sum of risk levels %d" sumOfRiskLevels
+    printf "Sum of risk levels %d\n" sumOfRiskLevels
 
     sumOfRiskLevels
 
 
 solve lines
 solve input
+
+
+let rec basinSize (map: int [,]) (x: int) (y: int) (basinCoordsList: list<int * int>) =
+    // printf "basinSize x %d y %d list %A\n" x y basinCoordsList
+
+    if (map.[x, y] = 9) then
+        // printf "\texit 9\n"
+        List.empty
+    else
+        // printf "\tincreaseBasen\n"
+        let neighbourhood = getNeighbourhood map x y
+
+        let updatedList = List.append basinCoordsList [ (x, y) ]
+
+        neighbourhood
+        |> List.filter (fun point -> not <| List.contains point updatedList)
+        |> List.map (fun (x, y) -> basinSize map x y updatedList)
+        |> List.concat
+        |> List.append updatedList
+// |> List.distinct
+
+
+
+let solve2 (input: array<string>) =
+    let intArray =
+        input
+        |> Array.map (fun line -> line.ToCharArray() |> Array.map charToInt)
+
+    let map =
+        Array2D.init<int> input.Length input.[0].Length (fun x y -> intArray.[x].[y])
+
+    let lowPointsMap =
+        map
+        |> Array2D.mapi (fun x y value ->
+            if (isLowPoint map x y) then
+                (value, x, y)
+            else
+                (9, x, y))
+
+    let lowPoints =
+        lowPointsMap
+        |> flat2Darray
+        |> Seq.filter (fun (depth, x, y) -> depth < 9)
+
+    printf "lowPoints %d\n" (Seq.length lowPoints)
+
+    let basinSizes =
+        lowPoints
+        // |> Seq.take 3
+        |> Seq.mapi (fun index (d, x, y) ->
+            printf "i %d d %d x %d y %d\n" index d x y
+
+            basinSize map x y List.empty
+            |> List.distinct
+            |> Seq.length)
+
+    let threeLargest =
+        basinSizes
+        |> Seq.sortDescending
+        |> Seq.take 3
+        |> Seq.toArray
+
+    let result =
+        threeLargest.[0]
+        * threeLargest.[1]
+        * threeLargest.[2]
+
+    printf "result %d\n" result
+    result
+
+// solve2 lines
+solve2 input
